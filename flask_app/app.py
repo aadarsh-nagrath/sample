2d66b232e8293b4ea2eec68edab5e4aef5495f64
Aadarsh Nagrath <92307537+aadarsh-nagrath@users.noreply.github.com>
2024-06-03 18:54:40 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_backend():
-    return 'Hello Backend'
+    return 'Hello Backend, is on maintance 🙃  syncing with git'
+
+@app.route('/webhook-handler', methods=['POST'])
+def webhook_handler():
+    data = request.json
+    if data['ref'] == 'refs/heads/main':  # Adjust according to your branch
+        pull_changes()
+    return '', 200
+
+def pull_changes():
+    repo_path = '/path/to/your/repo'  # Adjust this path
+    os.chdir(repo_path)
+    subprocess.run(['git', 'pull', 'origin', 'main'])  # Adjust according to your branch
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
