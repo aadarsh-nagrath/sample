36d050991d8691f6612d142e5116d769c053264b
Aadarsh Nagrath <92307537+aadarsh-nagrath@users.noreply.github.com>
2024-06-03 19:35:25 +0530

    test 1.8

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_backend():
-    return 'Hello Backend, is on maintance 🙃  syncing with github. Testing 1.6'
+    return 'Hello Backend, is on maintance 🙃  syncing with github. Testing 1.8'
 
 @app.route('/webhook-handler', methods=['POST'])
 def webhook_handler():
